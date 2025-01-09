20be9f6db608a63cc26c71d6108c100e1b2b8543
x100111010 <167847953+x100111010@users.noreply.github.com>
2025-01-10 00:48:43 +0100

    503 if db or node health is invalid

--- endpoints/get_health.py
+++ endpoints/get_health.py
@@ -9,6 +9,7 @@ from dbsession import async_session
 from models.Block import Block
 from server import app, kaspad_client
 from endpoints.get_virtual_chain_blue_score import current_blue_score_data
+from fastapi import HTTPException
 
 
 class KaspadResponse(BaseModel):
@@ -22,7 +23,7 @@ class KaspadResponse(BaseModel):
 class DBCheckStatus(BaseModel):
     status: str
     message: str
-    blueScoreDB: int = None  # holds db blue score if available
+    blueScoreDB: int = None
 
 
 class HealthResponse(BaseModel):
@@ -34,27 +35,25 @@ class HealthResponse(BaseModel):
 @app.get("/info/health", response_model=HealthResponse, tags=["Kaspa network info"])
 async def health_state():
     """
-    Checks the health of the node and database by comparing the latest blue scores from
-    both sources. The response includes the sync status, version, and blue score of the node,
-    as well as the latest blue score from the database. If the database blue score lags behind
-    the node's blue score by 1,000 or more, an "error" status is returned.
+    Checks node and database health by comparing blue score and sync status.
+    Returns health details or 503 if the database lags by 1,000+ blocks or a node is not synced.
     """
     await kaspad_client.initialize_all()
 
     kaspads = []
     db_check_status = DBCheckStatus(status="valid", message="Database blue score is within range")
 
-    # latest blue score from the node once
+    # latest blue score node
     current_blue_score_node = current_blue_score_data.get("blue_score")
 
-    # latest blue score from the database
+    # latest blue score db
     try:
         async with async_session() as s:
             last_blue_score_db = (
                 await s.execute(select(Block.blue_score).order_by(Block.blue_score.desc()).limit(1))
             ).scalar()
 
-        # check node and database blue scores
+        # check node and db blue scores
         if last_blue_score_db is None:
             db_check_status = DBCheckStatus(status="error", message="No blue score in database")
         elif current_blue_score_node is not None and abs(current_blue_score_node - last_blue_score_db) >= 1000:
@@ -63,7 +62,6 @@ async def health_state():
                 message=f"Blue score difference exceeds 1000 blocks (Node: {current_blue_score_node}, DB: {last_blue_score_db})",
             )
         else:
-            # If blue score difference is within 1000 blocks, mark as valid
             db_check_status = DBCheckStatus(
                 status="valid",
                 message="Database blue score is within range",
@@ -72,6 +70,26 @@ async def health_state():
 
     except Exception:
         db_check_status = DBCheckStatus(status="error", message="Database unavailable")
+
+    # 503 if db or node health is invalid
+    if db_check_status.status == "error" or not all(kaspad_info.is_synced for kaspad_info in kaspad_client.kaspads):
+        raise HTTPException(
+            status_code=503,
+            detail={
+                "kaspadServers": [
+                    {
+                        "isSynced": kaspad_info.is_synced,
+                        "isUtxoIndexed": kaspad_info.is_utxo_indexed,
+                        "p2pId": hashlib.sha256(kaspad_info.p2p_id.encode()).hexdigest(),
+                        "kaspadHost": f"KASPAD_HOST_{i + 1}",
+                        "serverVersion": kaspad_info.server_version,
+                    }
+                    for i, kaspad_info in enumerate(kaspad_client.kaspads)
+                ],
+                "currentBlueScoreNode": current_blue_score_node,
+                "currentDBStatus": db_check_status,
+            },
+        )
 
     for i, kaspad_info in enumerate(kaspad_client.kaspads):
         kaspads.append(
