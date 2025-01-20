7c6275d941cbed1063f1c549ef245704eeb19058
supertypo <suprtypo@pm.me>
2025-01-20 19:08:45 +0100

    Get max hashrate from kaspad if db dont have blocks

--- endpoints/get_hashrate.py
+++ endpoints/get_hashrate.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 import json
+import logging
 from datetime import datetime
 
 from pydantic import BaseModel
@@ -12,7 +13,7 @@ from helper.difficulty_calculation import bits_to_difficulty
 from models.Block import Block
 from server import app, kaspad_client
 
-MAXHASH_CACHE = (0, 0)
+_logger = logging.getLogger(__name__)
 
 
 class BlockHeader(BaseModel):
@@ -57,7 +58,6 @@ async def get_max_hashrate():
     """
     maxhash_last_value = json.loads((await KeyValueStore.get("maxhash_last_value")) or "{}")
     maxhash_last_bluescore = int((await KeyValueStore.get("maxhash_last_bluescore")) or 0)
-    print(f"Start at {maxhash_last_bluescore}")
 
     async with async_session() as s:
         block = (
@@ -69,24 +69,48 @@ async def get_max_hashrate():
             )
         ).scalar()
 
-    block_difficulty = bits_to_difficulty(block.bits)
-    hashrate_new = block_difficulty * 2
     hashrate_old = maxhash_last_value.get("blockheader", {}).get("difficulty", 0) * 2
-
-    await KeyValueStore.set("maxhash_last_bluescore", str(block.blue_score))
-
-    if hashrate_new > hashrate_old:
-        response = {
-            "hashrate": hashrate_new / 1_000_000_000_000,
-            "blockheader": {
-                "hash": block.hash,
-                "timestamp": datetime.fromtimestamp(block.timestamp / 1000).isoformat(),
-                "difficulty": block_difficulty,
-                "daaScore": block.daa_score,
-                "blueScore": block.blue_score,
-            },
-        }
-        await KeyValueStore.set("maxhash_last_value", json.dumps(response))
-        return response
+    logging.debug(f"hashrate_old: {int(hashrate_old)}")
+    if block:
+        block_difficulty = bits_to_difficulty(block.bits)
+        hashrate_new = block_difficulty * 2
+        logging.debug(f"hashrate_new (db): {int(hashrate_new)}")
+        await KeyValueStore.set("maxhash_last_bluescore", str(block.blue_score))
+        if hashrate_new > hashrate_old:
+            response = {
+                "hashrate": hashrate_new / 1_000_000_000_000,
+                "blockheader": {
+                    "hash": block.hash,
+                    "timestamp": datetime.fromtimestamp(block.timestamp / 1000).isoformat(),
+                    "difficulty": block_difficulty,
+                    "daaScore": block.daa_score,
+                    "blueScore": block.blue_score,
+                },
+            }
+            await KeyValueStore.set("maxhash_last_value", json.dumps(response))
+            return response
+    else:
+        resp = await kaspad_client.request("getBlockDagInfoRequest")
+        block_hash = resp["getBlockDagInfoResponse"]["virtualParentHashes"][0]
+        resp = await kaspad_client.request("getBlockRequest", params={"hash": block_hash, "includeTransactions": False})
+        block = resp.get("getBlockResponse", {}).get("block", {})
+        block_difficulty = int(block.get("verboseData", {}).get("difficulty", 0))
+        hashrate_new = block_difficulty * 2
+        logging.debug(f"hashrate_new (kaspad): {int(hashrate_new)}")
+        if hashrate_new > hashrate_old:
+            response = {
+                "hashrate": hashrate_new / 1_000_000_000_000,
+                "blockheader": {
+                    "hash": block.get("verboseData", {}).get("hash"),
+                    "timestamp": datetime.fromtimestamp(
+                        int(block.get("header", {}).get("timestamp", 0)) / 1000
+                    ).isoformat(),
+                    "difficulty": block_difficulty,
+                    "daaScore": int(block.get("header", {}).get("daaScore", 0)),
+                    "blueScore": int(block.get("header", {}).get("blueScore", 0)),
+                },
+            }
+            await KeyValueStore.set("maxhash_last_value", json.dumps(response))
+            return response
 
     return maxhash_last_value
