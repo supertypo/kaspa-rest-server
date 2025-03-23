5a66db2aef02e61d6624c5d322e52b7acb42668b
supertypo <suprtypo@pm.me>
2025-03-23 10:15:10 +0100

    get_health: Added env var HEALTH_TOLERANCE_DOWN (seconds)

--- endpoints/get_health.py
+++ endpoints/get_health.py
@@ -7,7 +7,7 @@ from fastapi import HTTPException
 from pydantic import BaseModel
 from sqlalchemy import select
 
-from constants import BPS
+from constants import BPS, HEALTH_TOLERANCE_DOWN
 from dbsession import async_session_blocks, async_session
 from endpoints.get_virtual_chain_blue_score import current_blue_score_data
 from models.Block import Block
@@ -55,7 +55,7 @@ async def health_state():
             db_check_status = DBCheckStatus(isSynced=False, blueScore=last_blue_score_db)
         else:
             blue_score_diff = abs(current_blue_score_node - last_blue_score_db)
-            is_synced = blue_score_diff < 600 * BPS
+            is_synced = blue_score_diff < HEALTH_TOLERANCE_DOWN * BPS
             db_check_status = DBCheckStatus(
                 isSynced=is_synced, blueScore=last_blue_score_db, blueScoreDiff=blue_score_diff
             )
@@ -69,7 +69,7 @@ async def health_state():
                 )
             ).scalar()
             time_diff = abs(int(time.time()) - int(last_accepted_tx_block_time_db) / 1000)
-            db_check_status.isSynced = db_check_status.isSynced and time_diff < 600
+            db_check_status.isSynced = db_check_status.isSynced and time_diff < HEALTH_TOLERANCE_DOWN
             db_check_status.acceptedTxBlockTime = last_accepted_tx_block_time_db
             db_check_status.acceptedTxBlockTimeDiff = time_diff
 
