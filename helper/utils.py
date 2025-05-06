40e809008ea4e528ea8df72e9fa9780c4318d2bc
supertypo <suprtypo@pm.me>
2025-05-07 00:34:06 +0200

    Add cache control based on abs value

--- helper/utils.py
+++ helper/utils.py
@@ -6,11 +6,12 @@ from endpoints.get_virtual_chain_blue_score import current_blue_score_data
 
 def add_cache_control(blue_score, timestamp, response):
     if blue_score and current_blue_score_data["blue_score"] > 0:
-        if int(blue_score) > current_blue_score_data["blue_score"] - 20 * BPS:
+        delta = abs(blue_score - current_blue_score_data["blue_score"]) / BPS
+        if delta < 20:
             response.headers["Cache-Control"] = "public, max-age=2"
-        elif int(blue_score) > current_blue_score_data["blue_score"] - 60 * BPS:
+        elif delta < 60:
             response.headers["Cache-Control"] = "public, max-age=10"
-        elif int(blue_score) > current_blue_score_data["blue_score"] - 600 * BPS:
+        elif delta < 600:
             response.headers["Cache-Control"] = "public, max-age=60"
         else:
             response.headers["Cache-Control"] = "public, max-age=600"
