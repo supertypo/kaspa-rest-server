5d70fa50af2648f32947806b192adc7736880605
supertypo <suprtypo@pm.me>
2025-05-29 17:44:50 +0200

    get_blockreward.py: support wrpc

--- endpoints/get_blockreward.py
+++ endpoints/get_blockreward.py
@@ -1,8 +1,9 @@
 # encoding: utf-8
-
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 from helper.deflationary_table import calc_block_reward
+from kaspad.KaspadRpcClient import kaspad_rpc_client
 from server import app, kaspad_client
 
 
@@ -15,13 +16,20 @@ async def get_blockreward(stringOnly: bool = False):
     """
     Returns the current blockreward in KAS/block
     """
-    resp = await kaspad_client.request("getBlockDagInfoRequest")
-    daa_score = int(resp["getBlockDagInfoResponse"]["virtualDaaScore"])
+    rpc_client = await kaspad_rpc_client()
+    if rpc_client:
+        bdi = await rpc_client.get_block_dag_info()
+    else:
+        resp = await kaspad_client.request("getBlockDagInfoRequest")
+        if resp.get("error"):
+            raise HTTPException(500, resp["error"])
+        bdi = resp["getBlockDagInfoResponse"]
+
+    daa_score = int(bdi["virtualDaaScore"])
     reward_info = calc_block_reward(daa_score)
     reward = reward_info["current"]
 
     if not stringOnly:
         return {"blockreward": reward}
-
     else:
         return f"{reward:.2f}"
