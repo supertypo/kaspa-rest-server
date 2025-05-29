20b1f2c0f0d816110edc8ff834763ef19bb413fb
supertypo <suprtypo@pm.me>
2025-05-29 18:07:54 +0200

    get_halving.py: support wrpc

--- endpoints/get_halving.py
+++ endpoints/get_halving.py
@@ -1,11 +1,13 @@
 # encoding: utf-8
 from datetime import datetime, timezone
 
+from fastapi import HTTPException
 from pydantic import BaseModel
 from starlette.responses import PlainTextResponse
 
 from constants import BPS
 from helper.deflationary_table import calc_block_reward
+from kaspad.KaspadRpcClient import kaspad_rpc_client
 from server import app, kaspad_client
 
 
@@ -20,8 +22,16 @@ async def get_halving(field: str | None = None):
     """
     Returns information about chromatic halving
     """
-    resp = await kaspad_client.request("getBlockDagInfoRequest")
-    daa_score = int(resp["getBlockDagInfoResponse"]["virtualDaaScore"])
+    rpc_client = await kaspad_rpc_client()
+    if rpc_client:
+        bdi = await rpc_client.get_block_dag_info()
+    else:
+        resp = await kaspad_client.request("getBlockDagInfoRequest")
+        if resp.get("error"):
+            raise HTTPException(500, resp["error"])
+        bdi = resp["getBlockDagInfoResponse"]
+
+    daa_score = int(bdi["virtualDaaScore"])
 
     reward_info = calc_block_reward(daa_score)
 
