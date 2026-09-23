332d5f07cf7339167c50ad3260152576a77b26ee
supertypo <suprtypo@pm.me>
2026-09-23 18:39:41 +0000

    Support Toccata transactions and payloads in transaction submission and mass

--- helper/tests/test_storage_mass.py
+++ helper/tests/test_storage_mass.py
@@ -2,7 +2,7 @@ from ..mass_calculation_storage import calc_storage_mass
 
 
 def test_storage_mass():
-    assert calc_storage_mass([1_0000_0000, 1_0000_0000], [9000_0000]) == 0
-    assert calc_storage_mass([1_0000_0000, 1_0000_0000], [9000_0000, 1000]) == 999991111
-    assert calc_storage_mass([1_1000_0000], [9000_0000, 1000, 1000, 1000]) == 3000002021
-    assert calc_storage_mass([1_0000_0000], [7900_0000, 2000_0000]) == 52658
+    assert calc_storage_mass([(1, 1_0000_0000), (1, 1_0000_0000)], [(1, 9000_0000)]) == 0
+    assert calc_storage_mass([(1, 1_0000_0000), (1, 1_0000_0000)], [(1, 9000_0000), (1, 1000)]) == 999991111
+    assert calc_storage_mass([(1, 1_1000_0000)], [(1, 9000_0000), (1, 1000), (1, 1000), (1, 1000)]) == 3000002021
+    assert calc_storage_mass([(1, 1_0000_0000)], [(1, 7900_0000), (1, 2000_0000)]) == 52658
